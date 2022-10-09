0770166225ae1310a70f9134a95f1dc3045d21ff
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2022-10-09 05:25:56 +0300

    edit the Videos component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,12 +1,21 @@
 import { Stack, Box } from "@mui/material";
+import { ChannelCard, VideoCard } from "./";
 
-const Videos = ({ videos }) => {
+import React from "react";
+
+const Videos = ({ videos}) => {
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item, index) => (
-        <Box key={index}>
+    <Stack
+      direction= "row"
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
-          {item.id.channelId && <ChannelCard channelId={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
